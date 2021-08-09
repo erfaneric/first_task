8f932d81cd352ddca69776c00bcd4acdda57c9b4
erfan <87976895+erfaneric@users.noreply.github.com>
2021-08-09 20:23:33 +0430

    Update my_first_task.py

--- my_first_task.py
+++ my_first_task.py
@@ -1,4 +1,4 @@
-def task_1(nums):
+def task_1(nums)->int:
     count = 0
     for i in range(len(nums)):
         if(is_prime(nums[i])):
@@ -7,7 +7,7 @@ def task_1(nums):
             continue
     return count
 
-def is_prime(n):
+def is_prime(n)->bool:
     for i in range(2,(n//2)+1):
         if(n % i ==0):
             return False
